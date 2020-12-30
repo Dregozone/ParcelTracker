cf763355267f721fe1d3a057b33eae0b10babde1
GlacialStudio <aclearmonth@gmail.com>
2020-12-30 22:18:12 +0000

    Fix weird situation where sellerViewDriverMetrics test would sometimes not find any results. Adds an example order and transactions so there is always at least one result in this situation now.

--- test/Seller_UI/ViewDriverMetricsCommandTest.java
+++ test/Seller_UI/ViewDriverMetricsCommandTest.java
@@ -2,6 +2,8 @@ package Seller_UI;
 
 import dto.MetricDTO;
 import java.util.ArrayList;
+import managedbean.DriverBean;
+import managedbean.SellerBean;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
@@ -14,6 +16,18 @@ public class ViewDriverMetricsCommandTest {
     public void testExecute() {
         System.out.println("___ SellerViewDriverMetrics execute");
 
+        // Create order so at least one order exists for this metric generation
+        SellerBean sellerInstance = new SellerBean();
+        int orderId = sellerInstance.getNextOrderId();
+        sellerInstance.setRecipientId(1);
+        sellerInstance.setSellerId(3);
+        sellerInstance.createOrder();
+        
+        // Add at least one completed order to allow valid metrics to be attained
+        DriverBean driverInstance = new DriverBean();
+        driverInstance.addTransaction(orderId, "Picked up", 2); // Assigns "driver" to this order
+        driverInstance.addTransaction(orderId, "Dropped off", 2); // Provides completed date for this order
+        
         ViewDriverMetricsCommand instance = new ViewDriverMetricsCommand();
 
         Object results = instance.execute();
@@ -31,8 +45,8 @@ public class ViewDriverMetricsCommandTest {
         boolean passed = true;
         
         if (
-            !metric.getName().equalsIgnoreCase("None") ||
-            metric.getDaysToComplete() != 2 /*||*/
+            !metric.getName().equalsIgnoreCase("driver") ||
+            metric.getDaysToComplete() != 0 /*||*/
             /* metric.getDeliveryCount() != 3 */ // Omit this count as it could change over time if wanting to test later
         ) {
             passed = false;
